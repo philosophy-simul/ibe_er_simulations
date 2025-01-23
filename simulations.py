869bbca1812b149cfacf8cccdd7cf00e96259bf1
philosophy-simul <8882862+philosophy-simul@users.noreply.github.com>
2025-01-23 22:06:05 +0100

    Add files via upload

--- simulations.py
+++ simulations.py
@@ -20,6 +20,8 @@ if __name__ == "__main__":
     thresholds = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     bias_lower = 0.1
     bias_upper = 0.9
+    weight_positive=2
+    weight_negative=1
     rule_labels = ['IBE_ER', 'IBE_St', 'JC', 'IBE*']
     marker_styles = ['o', 's', 'D', '^']  # Markers for each rule
     output_dir_plots = "plots-joint"
@@ -29,7 +31,7 @@ if __name__ == "__main__":
     # Run simulations and generate plots
     for ntosses in ntosses_list:
         print(f"Running simulations for n_tosses: {ntosses}")
-        results = run_simulations(ntosses, n_repetitions, bias_lower, bias_upper, partitioning, thresholds)
+        results = run_simulations(ntosses, n_repetitions, bias_lower, bias_upper, partitioning, thresholds,weight_positive,weight_negative)
         plot_results(results, partitioning, thresholds, bias_lower, bias_upper, rule_labels, output_dir_plots, ntosses)
         plot_mean_std(results, partitioning, bias_lower, bias_upper, rule_labels, marker_styles, output_dir_plots_single, ntosses)
         export_results_to_csv(results, partitioning, thresholds, bias_lower, bias_upper, rule_labels, output_dir_data, ntosses)
